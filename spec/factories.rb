7fd69580923393ddb7e426dd614c9f8d3a96901b
Rogelio Guzman <chinog9@gmail.com>
2011-01-25 08:01:57 -0600

    Revert "Can view offer details"
    This reverts commit 13023250293335e972683cef68eb7ba60434daaa.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,7 @@
 Factory.define :company do |f|
   f.title "My Company"
   f.city "Monterrey"
+  f.sequence(:email) {|n| "superuser#{n}@mycompany.com" }
   f.password "justme"
   f.password_confirmation "justme"
   f.description "This is my company"
