c99e69bad83c499e8601714e6b95f3912563a4f1
juanpabloe <juannpablo@gmail.com>
2011-01-26 14:34:05 -0600

    Working with the approach of required skills

--- db/migrate/20110106164317_create_required_skills.rb
+++ db/migrate/20110106164317_create_required_skills.rb
@@ -2,8 +2,6 @@ class CreateRequiredSkills < ActiveRecord::Migration
   def self.up
     create_table :required_skills do |t|
       t.string :skill_name
-      t.integer :job_id
-
       t.timestamps
     end
   end
