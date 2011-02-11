051644f3be609a68857820192454bc32c9db2896
Rogelio Guzman <rogelio@innku.com>
2011-02-11 09:16:50 -0600

    Preparing adding design

--- public/javascripts/tabbed-forms.js
+++ public/javascripts/tabbed-forms.js
@@ -3,12 +3,12 @@ $(function() {
 	number of fieldsets
 	*/
 	var fieldsetCount = $('#formElem').children().length;
-
+	
 	/*
 	current position of fieldset / navigation link
 	*/
 	var current 	= 1;
-
+    
 	/*
 	sum and save the widths of each one of the fieldsets
 	set the final sum as the total width of the steps element
@@ -21,19 +21,19 @@ $(function() {
         stepsWidth	 	+= $step.width();
     });
 	$('#steps').width(stepsWidth);
-
+	
 	/*
 	to avoid problems in IE, focus the first input of the form
 	*/
 	$('#formElem').children(':first').find(':input:first').focus();	
-
+	
 	/*
 	show the navigation bar
 	*/
 	$('#navigation').show();
-
+	
 	/*
-	when clicking on a navigation link
+	when clicking on a navigation link 
 	the form slides to the corresponding fieldset
 	*/
     $('#navigation a').bind('click',function(e){
@@ -43,14 +43,14 @@ $(function() {
         $this.parent().addClass('selected');
 		/*
 		we store the position of the link
-		in the current variable
+		in the current variable	
 		*/
 		current = $this.parent().index() + 1;
 		/*
 		animate / slide to the next or to the corresponding
 		fieldset. The order of the links in the navigation
 		is the order of the fieldsets.
-		Also, after sliding, we trigger the focus on the first
+		Also, after sliding, we trigger the focus on the first 
 		input element of the new fieldset
 		If we clicked on the last link (confirmation), then we validate
 		all the fieldsets, otherwise we validate the previous one
@@ -63,11 +63,11 @@ $(function() {
 				validateSteps();
 			else
 				validateStep(prev);
-			$('#formElem').children(':nth-child('+ parseInt(current) +')').find(':input:first').focus();
+			$('#formElem').children(':nth-child('+ parseInt(current) +')').find(':input:first').focus();	
 		});
         e.preventDefault();
     });
-
+	
 	/*
 	clicking on the tab (on the last input of each fieldset), makes the form
 	slide to the next step
@@ -83,10 +83,10 @@ $(function() {
 			}
 		});
 	});
-
+	
 	/*
 	validates errors on all the fieldsets
-	records if the form has errors in $('#formElem').data()
+	records if the Form has errors in $('#formElem').data()
 	*/
 	function validateSteps(){
 		var FormErrors = false;
@@ -95,42 +95,42 @@ $(function() {
 			if(error == -1)
 				FormErrors = true;
 		}
-		$('#formElem').data('errors',FormErrors);
+		$('#formElem').data('errors',FormErrors);	
 	}
-
+	
 	/*
 	validates one fieldset
 	and returns -1 if errors found, or 1 if not
 	*/
 	function validateStep(step){
 		if(step == fieldsetCount) return;
-
+		
 		var error = 1;
 		var hasError = false;
 		$('#formElem').children(':nth-child('+ parseInt(step) +')').find(':input:not(button)').each(function(){
 			var $this 		= $(this);
 			var valueLength = jQuery.trim($this.val()).length;
-
+			
 			if(valueLength == ''){
 				hasError = true;
 				$this.css('background-color','#FFEDEF');
 			}
 			else
-				$this.css('background-color','#FFFFFF');
+				$this.css('background-color','#FFFFFF');	
 		});
 		var $link = $('#navigation li:nth-child(' + parseInt(step) + ') a');
 		$link.parent().find('.error,.checked').remove();
-
+		
 		var valclass = 'checked';
 		if(hasError){
 			error = -1;
 			valclass = 'error';
 		}
 		$('<span class="'+valclass+'"></span>').insertAfter($link);
-
+		
 		return error;
 	}
-
+	
 	/*
 	if there are errors don't allow the user to submit
 	*/
@@ -138,7 +138,6 @@ $(function() {
 		if($('#formElem').data('errors')){
 			alert('Please correct the errors in the Form');
 			return false;
-		}
+		}	
 	});
 });
-
