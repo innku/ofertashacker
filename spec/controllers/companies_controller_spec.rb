f32ed5dcc51e71a27b8e0743cade1776001f8f3e
Rogelio Guzman <chinog9@gmail.com>
2011-04-07 11:23:41 -0500

    working with company specs

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe CompaniesController do
-  
+
   before do
     @company = Factory(:company)
+    controller.stub(:current_company).and_return(@company)
   end
 
   describe '#index' do
@@ -15,24 +16,66 @@ describe CompaniesController do
 
   describe '#show' do
     context 'With valid params' do
-      it 'Gets the company'
+      let(:valid_params){{:id => @company.id}}
+      it 'Gets the company' do
+        get :show, valid_params
+        assigns(:company).should eql(@company)
+      end
     end
 
     context 'With invalid params' do
-      it 'Raise an error'
-      it 'Does not get the company'
+      it 'Raise an error' do
+        lambda { get :show, {:id => 0} }.should raise_error
+      end
     end
   end
-  describe '#edit'
-  
+
+  describe '#edit' do
+
+    context 'With valid params' do
+      let(:valid_params){{:id => @company.id}}
+      it 'Gets the company' do
+        get :edit, valid_params
+        assigns(:company).should eql(@company)
+      end
+    end
+
+    context 'With invalid params' do
+      it 'Raise an error' do
+        lambda { get :edit, {:id => 0} }.should raise_error
+      end
+    end
+  end
+
   describe '#update' do
     context 'Valid attribute params' do
       it 'redirects to company profile'
     end
-    
+
     context 'Invalid attribute params' do
       it 'returns to editing the company data'
     end
   end
-  describe '#destroy'
+  describe '#destroy' do
+    let(:valid_params){{:id => @company.id}}
+    
+    it 'Gets the company' do
+      delete :destroy, valid_params
+      assigns(:company).should eql(@company)
+    end
+    
+    it 'Destroys the company' do
+      lambda{ delete :destroy, valid_params }.should change(Company, :count).by(-1)
+    end
+    
+    it 'Redirects to companies index' do
+      delete :destroy, valid_params
+      response.should redirect_to(companies_path)
+    end
+
+    it 'Renders a flash notice' do
+      delete :destroy, valid_params
+      flash[:notice].should_not be_blank
+    end
+  end
 end
