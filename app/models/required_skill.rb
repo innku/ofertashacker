6df57427e1af1331e6ef2db0ba312508ae5462d9
juanpabloe <juannpablo@gmail.com>
2011-04-13 17:12:03 -0500

    Removed skill categories

--- app/models/required_skill.rb
+++ app/models/required_skill.rb
@@ -1,9 +1,8 @@
 class RequiredSkill < ActiveRecord::Base
   
   belongs_to :job
-  belongs_to :skill_category
   validates_uniqueness_of :skill_name
 
-  validates_presence_of :skill_name, :skill_category_id
+  validates_presence_of :skill_name
   
 end
