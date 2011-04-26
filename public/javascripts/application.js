e29613e75546b076df8302db5aed2ba8acf390a3
Rogelio Guzman <chinog9@gmail.com>
2011-04-26 12:38:01 -0500

    changed compannias for empresas and fixed flash notice bug

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -12,22 +12,32 @@ $(document).ready(function() {
 	if($('#flash_notice') != null) {
     $('#flash_notice').append("<span id=\"close_notice\">X<\/span>")
     $('#flash_notice').hide().slideDown();
+    $('#close_notice').hide().slideDown();
 		setTimeout("$('#flash_notice').hide('blind',{},500);",4000);
+		setTimeout("$('#close_notice').hide('blind',{},500);",4000);
+
   }
-	if($('#flash_error') != null)
+	if($('#flash_error') != null) {
     $('#flash_error').append("<span id=\"close_notice\" >X<\/span>")
     $('#flash_error').hide().slideDown();
+    $('#close_notice').hide().slideDown();
 		setTimeout("$('#flash_error').hide('blind',{},500);",4000);
-	if($('#flash_alert') != null)
+		setTimeout("$('#close_notice').hide('blind',{},500);",4000);
+  }
+
+	if($('#flash_alert') != null) {
     $('#flash_alert').append("<span id=\"close_notice\">X<\/span>")
     $('#flash_alert').hide().slideDown();
+    $('#close_notice').hide().slideDown();
 		setTimeout("$('#flash_alert').hide('blind',{},500);",4000);
-    
+		setTimeout("$('#close_notice').hide('blind',{},500);",4000);
+  }
 	$('#close_notice').click(function() {
 
-    $('#flash_notice').remove();
-    $('#flash_error').remove();
-    $('#flash_alert').remove();
+    $('#flash_notice').slideUp();
+    $('#flash_error').slideUp();
+    $('#flash_alert').slideUp();
+    $('#close_notice').slideUp();
   });
 });
 //check whether a collection of checkboxes are checked or not
