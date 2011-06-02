07028a56652263b186c861a314aad86cd1bccba6
juanpabloe <juannpablo@gmail.com>
2011-06-02 13:57:49 -0500

    Expose jquery plugin agregado a la interaccion con lightboxes

--- public/javascripts/lightbox.js
+++ public/javascripts/lightbox.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-  $("a[rel]").overlay();
+  $("a[rel]").overlay({
+	mask: {
+		color: '#333',
+		loadSpeed: 150,
+		opacity: 0.9
+	},
+	closeOnClick: false
+  });
+  
   $("#sign_in").hide();
   $("#registration #login").click(function(){
      $("#registration").fadeOut({complete:function(){$("#sign_in").fadeIn();}});
