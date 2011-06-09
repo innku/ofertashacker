45a5f2cb33eefd1cf4c4058fb7d2073be6ae6221
juanpabloe <juannpablo@gmail.com>
2011-06-08 19:09:37 -0500

    Diseño implementado a la forma de registro de usuarios.

--- public/javascripts/lightbox.js
+++ public/javascripts/lightbox.js
@@ -9,8 +9,9 @@ $(document).ready(function() {
   });
 
   $("#sign_in").hide();
+  
   if ($("#sign_me_in").length > 0) {
-     $(".overlay.contact").animate({'width':'380px','marginLeft':'200px'});
+     $(".overlay.contact").animate({'width':'380px', 'height': '290px','marginLeft':'200px'});
       $("#registration").hide();
       $("#sign_in").show();
   }
@@ -18,13 +19,13 @@ $(document).ready(function() {
       $("#contact_button").click();
 
   $("#registration #login").click(function(){
-    $(".overlay.contact").animate({'width':'380px','marginLeft':'200px'});
+    $(".overlay.contact").animate({'width':'380px', 'height': '290px','marginLeft':'200px'});
     $("#registration").fadeOut({complete:function(){$("#sign_in").fadeIn();}});
 
   });
 
   $("#sign_in #register").click(function(){
-    $(".overlay.contact").animate({'width':'780px','marginLeft':'0'});
+    $(".overlay.contact").animate({'width':'780px', 'height':'460px','marginLeft':'0'});
     $("#sign_in").fadeOut({complete:function(){$("#registration").fadeIn();}});
   });
 });
