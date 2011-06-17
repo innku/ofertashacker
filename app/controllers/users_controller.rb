ce0724357aae23ce01ee4c5bdfc85e4adb3bd9a1
juanpabloe <juannpablo@gmail.com>
2011-06-17 10:41:02 -0500

    Acciones edit, update para users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,17 @@ class UsersController < ApplicationController
     ContactMailer.contact(job, @user).deliver if @user
     redirect_to @job
   end
+
+  def edit
+    @user = current_user 
+  end
+
+  def update
+    @user = current_user
+    if @user.update_attributes(params[:user])
+      redirect_to root_path
+    else
+      render :action => 'edit'
+    end
+  end
 end
