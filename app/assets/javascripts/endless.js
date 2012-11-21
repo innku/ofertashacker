0ceb31b7002ad858a97c2a02cf201e948ee2f1aa
e3matheus <e3matheus@gmail.com>
2012-11-21 16:46:08 -0600

    The filters should search with the keywords that are in the url also

--- app/assets/javascripts/endless.js
+++ app/assets/javascripts/endless.js
@@ -6,7 +6,11 @@ function getJobsJSON(filter_info,remove){
             part_time:filter_info[1],
             flexible:filter_info[2],
             remote:filter_info[3],
-  },jobs_ids:jobs_ids, location_id: $.urlParam('location_id'), location_type: $.urlParam('location_type')}, 
+  }, jobs_ids:jobs_ids, 
+     location_id: $.urlParam('location_id'), 
+     location_type: $.urlParam('location_type'),
+     keywords: $.urlParam('keywords')
+  }, 
   function(data) {
     changes = false;
     if(remove){
