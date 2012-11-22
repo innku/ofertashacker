990f8739f8716670dc7d8e7137a4d5a331934641
e3matheus <e3matheus@gmail.com>
2012-11-22 01:59:59 -0600

    [Refactor] Renamed the title of the like statement tests

--- spec/lib/helpers/like_statement_spec.rb
+++ spec/lib/helpers/like_statement_spec.rb
@@ -9,19 +9,19 @@ end
 describe 'LikeStatement' do
   describe 'to_array' do
 
-    it 'returns two sentence, the first is ILIKE of the field and the second the value you are searching' do
+    it 'returns a case insensitive search' do
       Helpers::LikeStatement.new(['title'], 'JP').to_array.should == ['title LIKE ?', '%JP%']
     end
 
-    it 'for each of the fields passed, it returns the two sentences mentioned before separated by an OR' do
+    it 'returns a case insensitive search for each of the fields passed' do
       Helpers::LikeStatement.new(['title', 'description'], 'JP').to_array.should == ['title LIKE ? OR description LIKE ?', '%JP%', '%JP%']
     end
 
-    it 'if passed an array of values it returns a case insensitive search for each of the values passed' do
+    it 'it returns a case insensitive search for each of the fields and values passed' do
       Helpers::LikeStatement.new(['title', 'description'], 'JP company').to_array.should == ['title LIKE ? OR title LIKE ? OR description LIKE ? OR description LIKE ?', '%JP%', '%company%','%JP%','%company%']
     end
 
-    it 'if passed a include null flag, for each of the fields passed, it returns a case insensitive query and a null value' do
+    it 'it returns a case insensitive or null query if the include_nulls flag is passed' do
       Helpers::LikeStatement.new(['title'], 'JP', true).to_array.should == ['title LIKE ? OR title IS NULL', '%JP%']
     end
   end
